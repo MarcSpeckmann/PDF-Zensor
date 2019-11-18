fa2417f4c7821cc0a2d9c3e0bdee8d7d9613a06a
Marc Speckmann <marc.speckmann@gmail.com>
2019-11-18 15:11:37 +0100

    PDF2-84 #comment autogenerated JUnit structure

---
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/config/CLArgsTest.java
@@ -0,0 +1,26 @@
+package de.uni_hannover.se.pdfzensor.config;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CLArgsTest {
+	
+	@BeforeEach
+	void setUp() {
+	}
+	
+	@AfterEach
+	void tearDown() {
+	}
+	
+	@Test
+	void fromStringArray() {
+	}
+	
+	@Test
+	void getVerbosity() {
+	}
+}
---
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/utils/PDFUtilsTest.java
@@ -0,0 +1,22 @@
+package de.uni_hannover.se.pdfzensor.utils;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PDFUtilsTest {
+	
+	@BeforeEach
+	void setUp() {
+	}
+	
+	@AfterEach
+	void tearDown() {
+	}
+	
+	@Test
+	void fitToArray() {
+	}
+}
