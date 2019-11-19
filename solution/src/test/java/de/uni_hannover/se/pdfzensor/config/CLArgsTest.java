f882166e2806bd057e2203d38b069aa3c0c1bb9a
Marc Speckmann <marc.speckmann@gmail.com>
2019-11-19 15:40:14 +0100

    PDF2-84 #comment added test for CLArgs

--- solution/src/test/java/de/uni_hannover/se/pdfzensor/config/CLArgsTest.java
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/config/CLArgsTest.java
@@ -1,23 +1,37 @@
 package de.uni_hannover.se.pdfzensor.config;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.apache.logging.log4j.Level;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static de.uni_hannover.se.pdfzensor.Logging.VERBOSITY_LEVELS;
 
 class CLArgsTest {
 	
 	@Test
 	void fromStringArray() {
 		assertThrows(IllegalArgumentException.class, () -> CLArgs.fromStringArray(null));
-		assertNotNull(CLArgs.fromStringArray(new String[0]));
+		assertNotNull(CLArgs.fromStringArray());
+		assertEquals(CLArgs.class, CLArgs.fromStringArray().getClass());
 		//Todo: add more tests
 	}
 	
 	@Test
 	void getVerbosity() {
 		//TODO: add tests
-		fail("No tests implemented yet");
+		var cla = CLArgs.fromStringArray();
+		//assertEquals(Level.OFF, cla.getVerbosity());
+		String input = "-v";
+		for (int i = 1; i < VERBOSITY_LEVELS.length; i++) {
+			cla = CLArgs.fromStringArray("-v");
+			assertEquals(Level.FATAL, cla.getVerbosity());
+			input = input.concat("v");
+		}
+		
+		cla = CLArgs.fromStringArray("-vvvvvvv");
+		assertEquals(Level.ALL, cla.getVerbosity());
+		
+		cla = CLArgs.fromStringArray("-vvvvvvvv");
+		assertEquals(Level.ALL, cla.getVerbosity());
 	}
 }
