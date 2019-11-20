c375e56d2d92f35b4454c4cadb2e6cc76504d569
Lennart Bohlin <l.bohlin@gmx.de>
2019-11-20 11:45:30 +0100

    Apply suggestion to solution/src/main/java/de/uni_hannover/se/pdfzensor/config/CLArgs.java

--- solution/src/main/java/de/uni_hannover/se/pdfzensor/config/CLArgs.java
+++ solution/src/main/java/de/uni_hannover/se/pdfzensor/config/CLArgs.java
@@ -19,7 +19,7 @@ import static de.uni_hannover.se.pdfzensor.utils.Utils.fitToArray;
  * @author Lennart Bohlin
  */
 @Command(name = "pdf-zensor", version = DummyVersionProvider.VERSION, description = {"--Here could be your description--"})
-public final class CLArgs {
+final class CLArgs {
 	
 	@Option(names = {"-v", "--verbose"}, description = {"Specify multiple -v options to increase verbosity."}, arity = "0")
 	@Nullable
