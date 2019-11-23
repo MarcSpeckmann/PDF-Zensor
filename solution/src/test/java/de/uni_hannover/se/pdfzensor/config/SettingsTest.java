468596a4fb8b8c5dc3f48d847b1e74cac87798e2
Tim Hagen <timhagenmail@googlemail.com>
2019-11-23 08:22:58 +0100

    Updated testing of the color-codes to be parameterized and more thorough

--- solution/src/test/java/de/uni_hannover/se/pdfzensor/config/SettingsTest.java
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/config/SettingsTest.java
@@ -1,91 +1,105 @@
 package de.uni_hannover.se.pdfzensor.config;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.awt.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Stream;
 
+import static de.uni_hannover.se.pdfzensor.config.Settings.colorToString;
 import static org.junit.jupiter.api.Assertions.*;
 
 
 /** SettingsTest should contain all unit-tests related to {@link Settings}. */
 class SettingsTest {
-
+	private static final String[] COLOR_PREFIXES = {"0X", "0x", "#"};
+	private static final Map<Color, String[]> COLORS = new HashMap<>();
+	
+	static {
+		COLORS.put(Color.black, new String[]{"000", "000000"});
+		COLORS.put(Color.red, new String[]{"f00", "ff0000"});
+		COLORS.put(Color.green, new String[]{"0f0", "00ff00"});
+		COLORS.put(Color.blue, new String[]{"00f", "0000ff"});
+		COLORS.put(Color.yellow, new String[]{"ff0", "ffff00"});
+		COLORS.put(Color.cyan, new String[]{"0ff", "00ffff"});
+		COLORS.put(Color.magenta, new String[]{"f0f", "ff00ff"});
+		COLORS.put(Color.white, new String[]{"fff", "ffffff"});
+		COLORS.put(Color.DARK_GRAY, new String[]{"404040"});
+		COLORS.put(Color.GRAY, new String[]{"808080"});
+		// some random colors
+		COLORS.put(new Color(130, 150, 161), new String[]{"8296A1"});
+		COLORS.put(new Color(77, 52, 67), new String[]{"4D3443"});
+		COLORS.put(new Color(18, 10, 77), new String[]{"120A4D"});
+		COLORS.put(new Color(18, 52, 86), new String[]{"123456"});
+		COLORS.put(new Color(3, 77, 31), new String[]{"034D1F"});
+		COLORS.put(new Color(77, 76, 27), new String[]{"4D4C1B"});
+		COLORS.put(new Color(86, 42, 86), new String[]{"562A56"});
+		COLORS.put(new Color(250, 204, 204), new String[]{"FACCCC"});
+	}
+	
+	/** Provides a set of arguments for {@link #testColorCode(String[], Color)} generated from {@link #COLORS}. */
+	private static Stream<Arguments> colorCodeProvider() {
+		return COLORS.entrySet()
+					 .stream()
+					 .map(e -> Arguments.of(e.getValue(), e.getKey()));
+	}
+	
 	/** Unit-tests for {@link Settings} constructor Settings */
 	@Test
-	void testSettings(){
-        // if the command line argument is not given or has a faulty structure
+	void testSettings() {
+		// if the command line argument is not given or has a faulty structure
 		//TODO the following test does not Work yet
 		//assertThrows(NullPointerException.class, () -> new Settings(null));
-        assertThrows(IllegalArgumentException.class, () -> new Settings(new String[2]));
-        // if the command line argument is given but not valid
-        // split uses whitespace as delimiter and splits the single string into an array of multiple strings for using it as an argument
+		assertThrows(IllegalArgumentException.class, () -> new Settings(new String[2]));
+		// if the command line argument is given but not valid
+		// split uses whitespace as delimiter and splits the single string into an array of multiple strings for using it as an argument
 		//
-		assertThrows(picocli.CommandLine.UnmatchedArgumentException.class, () -> new Settings("pdf-zensor", "\"NichtExistenteDatei.pdf\""));
-        // for this test there has to be a zensieren.pdf file in the same directory but no config.json
-        assertThrows(picocli.CommandLine.UnmatchedArgumentException.class, () -> new Settings("pdf-zensor", "\"zensieren.pdf\"", "-c", "\"config.json\""));
+		assertThrows(picocli.CommandLine.UnmatchedArgumentException.class,
+					 () -> new Settings("pdf-zensor", "\"NichtExistenteDatei.pdf\""));
+		// for this test there has to be a zensieren.pdf file in the same directory but no config.json
+		assertThrows(picocli.CommandLine.UnmatchedArgumentException.class,
+					 () -> new Settings("pdf-zensor", "\"zensieren.pdf\"", "-c", "\"config.json\""));
 	}
-
+	
+	/**
+	 * Checks if the colorCodes all produce the expected color and if the expected color will be converted into one of
+	 * the color-codes. The color-codes will be prepended with each of the {@link #COLOR_PREFIXES}.
+	 */
+	@ParameterizedTest(name = "Run {index}: ColorCodes: {0}")
+	@MethodSource("colorCodeProvider")
+	void testColorCode(String[] colorCodes, Color expected) {
+		//Check if getColorOrNull works correctly
+		for (String code : colorCodes)
+			for (String pre : COLOR_PREFIXES)
+				assertEquals(expected, Settings.getColorOrNull(pre + code));
+		//Check if colorToString works correctly
+		var actual = colorToString(expected);
+		boolean valid = false;
+		for (String code : colorCodes) {
+			for (String pre : COLOR_PREFIXES) {
+				if (actual.equalsIgnoreCase(pre + code)) {
+					valid = true;
+					break;
+				}
+			}
+			if (valid) break;
+		}
+		assertTrue(valid, "Expected one of: #" + Arrays.toString(colorCodes) + " but was: " + actual);
+		
+	}
+	
 	/** Unit-tests for {@link Settings} function getColorOrNull */
 	@Test
-	void getColorOrNull() {
-		// if the parameter is null
+	void testIllegalColors() {
+		// check if a null-argument results in null
 		assertNull(Settings.getColorOrNull(null));
 		
-		//if the parameter is a valid hexadecimal color code with 6 letters beginning with '#'
-		assertEquals(Color.BLACK, Settings.getColorOrNull("#000000"));
-		assertEquals(Color.RED, Settings.getColorOrNull("#ff0000"));
-		assertEquals(Color.GREEN, Settings.getColorOrNull("#00ff00"));
-		assertEquals(Color.BLUE, Settings.getColorOrNull("#0000ff"));
-		assertEquals(Color.YELLOW, Settings.getColorOrNull("#ffff00"));
-		assertEquals(Color.CYAN, Settings.getColorOrNull("#00ffff"));
-		assertEquals(Color.WHITE, Settings.getColorOrNull("#ffffff"));
-		
-		//if the parameter is a valid hexadecimal color code with 3 letters beginning with '#'
-		assertEquals(Color.BLACK, Settings.getColorOrNull("#000"));
-		assertEquals(Color.RED, Settings.getColorOrNull("#f00"));
-		assertEquals(Color.GREEN, Settings.getColorOrNull("#0f0"));
-		assertEquals(Color.BLUE, Settings.getColorOrNull("#00f"));
-		assertEquals(Color.YELLOW, Settings.getColorOrNull("#ff0"));
-		assertEquals(Color.CYAN, Settings.getColorOrNull("#0ff"));
-		assertEquals(Color.WHITE, Settings.getColorOrNull("#fff"));
-		
-		// if the parameter is a valid hexadecimal color with 6 letters beginning with '0x'
-		assertEquals(Color.BLACK, Settings.getColorOrNull("0x000000"));
-		assertEquals(Color.RED, Settings.getColorOrNull("0xff0000"));
-		assertEquals(Color.GREEN, Settings.getColorOrNull("0x00ff00"));
-		assertEquals(Color.BLUE, Settings.getColorOrNull("0x0000ff"));
-		assertEquals(Color.YELLOW, Settings.getColorOrNull("0xffff00"));
-		assertEquals(Color.CYAN, Settings.getColorOrNull("0x00ffff"));
-		assertEquals(Color.WHITE, Settings.getColorOrNull("0xffffff"));
-		// the same with 'OX'
-		assertEquals(Color.BLACK, Settings.getColorOrNull("0X000000"));
-		assertEquals(Color.RED, Settings.getColorOrNull("0Xff0000"));
-		assertEquals(Color.GREEN, Settings.getColorOrNull("0X00ff00"));
-		assertEquals(Color.BLUE, Settings.getColorOrNull("0X0000ff"));
-		assertEquals(Color.YELLOW, Settings.getColorOrNull("0Xffff00"));
-		assertEquals(Color.CYAN, Settings.getColorOrNull("0X00ffff"));
-		assertEquals(Color.WHITE, Settings.getColorOrNull("0Xffffff"));
-		
-		//if the parameter is a valid hexadecimal color code with 3 letters beginning with '0x'
-		assertEquals(Color.BLACK, Settings.getColorOrNull("0x000"));
-		assertEquals(Color.RED, Settings.getColorOrNull("0xf00"));
-		assertEquals(Color.GREEN, Settings.getColorOrNull("0x0f0"));
-		assertEquals(Color.BLUE, Settings.getColorOrNull("0x00f"));
-		assertEquals(Color.YELLOW, Settings.getColorOrNull("0xff0"));
-		assertEquals(Color.CYAN, Settings.getColorOrNull("0x0ff"));
-		assertEquals(Color.WHITE, Settings.getColorOrNull("0xfff"));
-		assertEquals(Color.DARK_GRAY, Settings.getColorOrNull("#404040"));
-
-		// some random colors
-		assertEquals(new Color(130,150,161), Settings.getColorOrNull("#8296A1"));
-		assertEquals(new Color(77,52,67), Settings.getColorOrNull("0x4D3443"));
-		assertEquals(new Color(18, 10, 77), Settings.getColorOrNull("0x120A4D"));
-		assertEquals(new Color(18, 52, 86), Settings.getColorOrNull("0x123456"));
-		assertEquals(new Color(3, 77, 31), Settings.getColorOrNull("0x034D1F"));
-		assertEquals(new Color(77, 76, 27), Settings.getColorOrNull("0x4D4C1B"));
-		assertEquals(new Color(86, 42, 86), Settings.getColorOrNull("0x562A56"));
-		assertEquals(new Color(250, 204, 204), Settings.getColorOrNull("#FACCCC"));
-		// if the parameter is no valid hexadecimal color code
+		// check if invalid color-codes are detected
 		assertThrows(IllegalArgumentException.class, () -> Settings.getColorOrNull("BLACK"));
 		assertThrows(IllegalArgumentException.class, () -> Settings.getColorOrNull("#f"));
 		assertThrows(IllegalArgumentException.class, () -> Settings.getColorOrNull("#ff"));
@@ -94,26 +108,10 @@ class SettingsTest {
 		assertThrows(IllegalArgumentException.class, () -> Settings.getColorOrNull("#ffffgg"));
 		assertThrows(IllegalArgumentException.class, () -> Settings.getColorOrNull("0xffffgg"));
 		assertThrows(IllegalArgumentException.class, () -> Settings.getColorOrNull("#f3875323"));
-        assertThrows(IllegalArgumentException.class, () -> Settings.getColorOrNull("foo"));
-        assertThrows(IllegalArgumentException.class, () -> Settings.getColorOrNull("0Xkkkkk"));
-        assertThrows(IllegalArgumentException.class, () -> Settings.getColorOrNull("1Xffffff"));
-        assertThrows(IllegalArgumentException.class, () -> Settings.getColorOrNull("#varargs"));
-        assertThrows(IllegalArgumentException.class, () -> Settings.getColorOrNull("#a color"));
-	}
-
-	/*
-	* Unit-tests for {@link colorToString}
-	*  */
-	@Test
-	void colorToString() {
-		
-		// if the parameter is a Color class
-		assertEquals("#000000", Settings.colorToString(Color.BLACK));
-		assertEquals("#FF0000", Settings.colorToString(Color.RED));
-		assertEquals("#00FF00", Settings.colorToString(Color.GREEN));
-		assertEquals("#0000FF", Settings.colorToString(Color.BLUE));
-		assertEquals("#FFFF00", Settings.colorToString(Color.YELLOW));
-		assertEquals("#00FFFF", Settings.colorToString(Color.CYAN));
-		assertEquals("#FFFFFF", Settings.colorToString(Color.WHITE));
+		assertThrows(IllegalArgumentException.class, () -> Settings.getColorOrNull("foo"));
+		assertThrows(IllegalArgumentException.class, () -> Settings.getColorOrNull("0Xkkkkk"));
+		assertThrows(IllegalArgumentException.class, () -> Settings.getColorOrNull("1Xffffff"));
+		assertThrows(IllegalArgumentException.class, () -> Settings.getColorOrNull("#varargs"));
+		assertThrows(IllegalArgumentException.class, () -> Settings.getColorOrNull("#a color"));
 	}
 }
