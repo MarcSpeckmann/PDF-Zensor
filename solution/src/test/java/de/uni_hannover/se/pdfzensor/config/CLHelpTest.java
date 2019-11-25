b39d4dc24dc481a4c380dfbae28feda3d4d958ed
Marc Speckmann <marc.speckmann@gmail.com>
2019-11-25 14:07:55 +0100

    PDF2-35 #comment Added TODO's

---
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/config/CLHelpTest.java
@@ -0,0 +1,12 @@
+package de.uni_hannover.se.pdfzensor.config;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CLHelpTest {
+	
+	@Test
+	void printStandardHelpOptionsIfRequested() {
+	}
+}
