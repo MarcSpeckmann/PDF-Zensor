26e2dafe8c7b27cb3783480c665282363eef1c75
Lennart Bohlin <l.bohlin@gmx.de>
2019-11-26 13:56:41 +0100

    pdf2-84 #comment tests for versionProvider

--- solution/src/test/java/de/uni_hannover/se/pdfzensor/config/VersionProviderTest.java
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/config/VersionProviderTest.java
@@ -4,19 +4,49 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Properties;
+
 /**
  * TODO: Add JavaDoc
  */
 class VersionProviderTest {
 	
 	/**
-	 * TODO: ADD JavaDoc
+	 * Automatised tests to check if version and timestamp exist in properties.
+	 */
+	@Test
+	void getProperties() {
+		final Properties properties = new Properties();
+		try {
+			properties.load(Objects.requireNonNull(this.getClass()
+													   .getResourceAsStream("/project.properties")));
+		} catch (IOException ex) {
+			fail("failed to load properties");
+		}
+		assertNotNull(properties);
+		assertNotNull(properties.getProperty("version"));
+		assertNotNull(properties.getProperty("timestamp"));
+	}
+	
+	/**
+	 * Automatised test that checks if the getVersion function returns parameters that are longer than their
+	 * description.
 	 */
 	@Test
 	void getVersion() {
 		var versionProvider = new VersionProvider();
 		assertDoesNotThrow(versionProvider::getVersion);
-		assertNotNull(versionProvider.getVersion());
-		assertTrue(versionProvider.getVersion().length > 0, "The version output has no lines");
+		try {
+			String[] version = versionProvider.getVersion();
+			assertNotNull(version);
+			assertTrue(version[1].length() > "Version: ".length());
+			assertTrue(version[2].length() > "Build: ".length());
+		} catch (IOException ex) {
+			fail("failed to load VersionProvider");
+		}
 	}
+	
+	
 }
