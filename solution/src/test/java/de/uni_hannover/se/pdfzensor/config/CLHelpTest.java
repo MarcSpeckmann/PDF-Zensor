d27af569eb10795bae4fccb06dc378840af60c69
Lennart Bohlin <l.bohlin@gmx.de>
2019-11-26 14:44:41 +0100

    PDF2-84 #comment added CLHelp tests

--- solution/src/test/java/de/uni_hannover/se/pdfzensor/config/CLHelpTest.java
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/config/CLHelpTest.java
@@ -1,6 +1,12 @@
 package de.uni_hannover.se.pdfzensor.config;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,13 +14,25 @@ import static org.junit.jupiter.api.Assertions.*;
  * CLHelpTest should contain all unit-tests related to {@link CLHelp}.
  */
 class CLHelpTest {
+
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	private final PrintStream originalOut = System.out;
 	
-	/**
-	 * TODO: Add JavaDocs
+	@BeforeAll
+	public void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+	}
+	
+	@AfterAll
+	public void restoreStreams() {
+		System.setOut(originalOut);
+	}
+	 
+	 /** Tests if a call including -V or -h prints text.
 	 */
-	@Test
+	@ParameterizedTest
 	void printStandardHelpOptionsIfRequested() {
-		//TODO: implement tests
-		fail("No tests implemented yet");
+		assertNotNull(CLHelp.printStandardHelpOptionsIfRequested());
+		assertTrue(outContent.toString().length() > 10);
 	}
 }
