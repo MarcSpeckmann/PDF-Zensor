9471e77dfd522423773432cb93703e31c34e953d
Marc Speckmann <marc.speckmann@gmail.com>
2019-11-27 21:01:42 +0100

    Added test structure

---
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/config/CLErrorMessageHandlerTest.java
@@ -0,0 +1,21 @@
+package de.uni_hannover.se.pdfzensor.config;
+
+import org.junit.jupiter.api.Test;
+import picocli.CommandLine;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * CLHelpTest should contain all unit-tests related to {@link CLErrorMessageHandler}.
+ */
+class CLErrorMessageHandlerTest {
+	
+	/**
+	 * Testing if {@link CommandLine.ParameterException} is handled the right way.
+	 */
+	@Test
+	void handleParseException() {
+		//TODO:Add tests
+		fail("Test not implemented yet");
+	}
+}
