e2d5a1cab2ed9c9ac66f5b6593fed229617c49d4
Tim Hagen <timhagenmail@googlemail.com>
2019-11-27 13:17:45 +0100

    Update CLHelp.java -- fixed a typo

--- solution/src/main/java/de/uni_hannover/se/pdfzensor/config/CLHelp.java
+++ solution/src/main/java/de/uni_hannover/se/pdfzensor/config/CLHelp.java
@@ -20,7 +20,7 @@ public final class CLHelp {
 	}
 	
 	/**
-	 * Prints help or Version if -h either -V is present in the arguments.
+	 * Prints help or version if either -h or -V is present in the arguments.
 	 *
 	 * @param args the command-line arguments which will be parsed
 	 * @return true if help or version is requested
