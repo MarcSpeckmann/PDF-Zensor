e0fac13956884d795d850ce198ba1acb40c68d1b
Tim Hagen <timhagenmail@googlemail.com>
2019-11-28 08:52:12 +0100

    Refactored CLHelp

--- solution/src/main/java/de/uni_hannover/se/pdfzensor/config/CLHelp.java
+++ solution/src/main/java/de/uni_hannover/se/pdfzensor/config/CLHelp.java
@@ -3,48 +3,40 @@ package de.uni_hannover.se.pdfzensor.config;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import picocli.CommandLine;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.ParseResult;
 
-/**
- * This class is responsible for printing version and help if requested.
- */
-@Command(mixinStandardHelpOptions = true)
+import java.util.Objects;
+
+/** This class is responsible for printing the version and help if requested. */
 public final class CLHelp {
+	
 	/**
 	 * The default constructor should not be called and thus will always throw an exception when called.
+	 *
+	 * @throws UnsupportedOperationException when called.
 	 */
-	@Contract(pure = true)
+	@Contract(value = " -> fail", pure = true)
 	private CLHelp() {
 		throw new UnsupportedOperationException();
 	}
 	
 	/**
-	 * Prints help or version if either -h or -V is present in the arguments.
+	 * Prints the help- or version-string as they are defined in {@link CLArgs} to stdout. If the help-argument (-h or
+	 * --help) is present in the arguments, the usage text is printed. If the version-argument (-V or --Version) is
+	 * present in the arguments, the version-string for {@link CLArgs} is printed. If both are present only the usage is
+	 * printed.
 	 *
 	 * @param args the command-line arguments which will be parsed
 	 * @return true if help or version is requested
 	 */
-	public static boolean printStandardHelpOptionsIfRequested(String... args) {
-		var cmd = new CommandLine(CLArgs.class);
-		if (isVersionOrHelpRequested(cmd.parseArgs(args))) {
-			if (cmd.isUsageHelpRequested()) {
-				new CommandLine(CLArgs.class).usage(System.out);
-			} else if (cmd.isVersionHelpRequested()) {
-				new CommandLine(CLArgs.class).printVersionHelp(System.out);
-			}
-			return true;
-		}
-		return false;
-	}
-	
-	/**
-	 * Returns true if help or version is requested.
-	 *
-	 * @param pr parsed commando-line arguments
-	 * @return true if help or version is requested
-	 */
-	private static boolean isVersionOrHelpRequested(@NotNull ParseResult pr) {
-		return pr.isUsageHelpRequested() || pr.isVersionHelpRequested();
+	@SuppressWarnings("squid:S106")// we explicitly want to print to stdout here instead of logging
+	public static boolean printStandardHelpOptionsIfRequested(@NotNull String... args) {
+		final var cmd = new CommandLine(CLArgs.class);
+		final var parsedHelp = new CommandLine(CLArgs.class).parseArgs(Objects.requireNonNull(args));
+		if (parsedHelp.isUsageHelpRequested())
+			cmd.usage(System.out);
+		else if (parsedHelp.isVersionHelpRequested())
+			cmd.printVersionHelp(System.out);
+		else return false;
+		return true;
 	}
 }
