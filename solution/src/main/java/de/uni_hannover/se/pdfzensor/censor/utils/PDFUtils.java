974181d926765bff925b89436866408d340746e3
Lennart Möller <moeller.lennart@gmail.com>
2019-11-28 18:59:07 +0100

    #PDF2-94 #comment implemented PDFUtils and wrote JavaDoc

--- solution/src/main/java/de/uni_hannover/se/pdfzensor/censor/utils/PDFUtils.java
+++ solution/src/main/java/de/uni_hannover/se/pdfzensor/censor/utils/PDFUtils.java
@@ -1,27 +1,68 @@
 package de.uni_hannover.se.pdfzensor.censor.utils;
 
+import org.apache.fontbox.util.BoundingBox;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDType3Font;
 import org.apache.pdfbox.text.TextPosition;
+
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
+import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.awt.geom.Rectangle2D;
 
+/**
+ * PDFUtils is a utility class for the censor package and only provides {@link #transformTextPosition(TextPosition)} to
+ * the outside of the package
+ */
 public final class PDFUtils {
 	
+	/**
+	 * Constructor of PDFUtils not usable!
+	 *
+	 * @throws UnsupportedOperationException thrown when trying to use the constructor
+	 */
 	@Contract(value = " -> fail", pure = true)
-	private PDFUtils() {
-	
+	private PDFUtils() throws UnsupportedOperationException {
+		throw new UnsupportedOperationException();
 	}
 	
+	/**
+	 * Generates a Rectangle2D element with the properties of a PDRectangle
+	 * @param rect A rectangle from type PDRectangle with height, width and position
+	 * @return A rectangle from type Rectangle2D with the properties of the input rectangle
+	 */
 	@NotNull
 	@Contract("_ -> new")
 	static Rectangle2D pdrectToRect2D(@NotNull PDRectangle rect) {
-		return null;
+		return new Rectangle2D.Float(rect.getLowerLeftX(), rect.getLowerLeftY(), rect.getWidth(), rect.getHeight());
 	}
 	
+	
+	/**
+	 * Generates a rectangle from type Rectangle2D with the height, width and position of a given text line
+	 * @param pos A TextPosition representing a string and a position of characters on the screen
+	 * @return A rectangle from type Rectangle2D with the properties of height, width and position from the
+	 * 		   TextPosition or returns a Rectangle2D with height, width and position equals 0 if an error occurred
+	 */
 	public static Rectangle2D transformTextPosition(TextPosition pos) {
-		return null;
+		try {
+			PDFont font = pos.getFont();
+			BoundingBox bb = font.getBoundingBox();
+			int totalWidth = 0;        // total width of all characters in this line
+			for (int i : pos.getCharacterCodes())
+				totalWidth += font.getWidth(i);
+			AffineTransform at = pos.getTextMatrix().createAffineTransform();
+			if (font instanceof PDType3Font)    // specific type of font
+				at.concatenate(font.getFontMatrix().createAffineTransform());
+			else at.scale(1 / 1000f, 1 / 1000f);
+			Rectangle2D r = new Rectangle2D.Float(0, 0, totalWidth, bb.getHeight() + bb.getLowerLeftY());
+			Shape s = at.createTransformedShape(r);
+			return s.getBounds2D();
+		} catch (Exception ignored) {/**/}
+		return new Rectangle2D.Float(0, 0, 0, 0); // if an error occurred
 	}
 	
 }
