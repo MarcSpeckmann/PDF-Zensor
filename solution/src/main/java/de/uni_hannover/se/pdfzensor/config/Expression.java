ddb2fd44078d678cbef96d904ea856b2b07a4ded
Tim Hagen <timhagenmail@googlemail.com>
2019-11-28 09:22:26 +0100

    Refactored Expression

--- solution/src/main/java/de/uni_hannover/se/pdfzensor/config/Expression.java
+++ solution/src/main/java/de/uni_hannover/se/pdfzensor/config/Expression.java
@@ -12,21 +12,27 @@ import java.util.Objects;
 import static de.uni_hannover.se.pdfzensor.utils.Utils.*;
 
 /**
- * A class containing regular expressions and information on the color with which to censor text which matches the
- * regular expression.
+ * The Expression class represents a regex-color-tuple that may be defined in the configuration file or with the command
+ * line arguments. When a text that matches the stored regex is censored within a pdf-file it should be censored in the
+ * provided color.
  */
 final class Expression {
-	
 	@NotNull
 	private final String regex;
 	@Nullable
 	private Color color;
 	
 	/**
-	 * An Expression object may be initialized without a color, in which case one would be assigned later.
+	 * Initializes a new expression object with the provided regular expression and color (in hexadecimal notation). The
+	 * provided color may be null. The color can than be set at a later time. The regex on the other hand may not be
+	 * null and can not be changed later on.
 	 *
-	 * @param regex    This Expression's regex pattern.
-	 * @param hexColor This Expression's color.
+	 * @param regex    The regex that should be matched to use this expression's color for censoring.
+	 * @param hexColor The color that should be used to censor a text that matches this expression.
+	 * @throws NullPointerException     if regex is null
+	 * @throws IllegalArgumentException if the hexColor is not null and incorrectly formatted
+	 * @see de.uni_hannover.se.pdfzensor.utils.Utils#colorToString(Color)
+	 * @see de.uni_hannover.se.pdfzensor.utils.Utils#getColorOrNull(String)
 	 */
 	@JsonCreator()
 	Expression(@NotNull @JsonProperty("regex") final String regex,
@@ -36,6 +42,8 @@ final class Expression {
 	}
 	
 	/**
+	 * Returns the regex that is used to match this expression in text.
+	 *
 	 * @return The regular expression associated with this object.
 	 */
 	@Contract(pure = true)
@@ -45,7 +53,10 @@ final class Expression {
 	}
 	
 	/**
-	 * @return The color associated with this object.
+	 * Returns the color that should be used to censor a text-passage that matches this expression's regex.
+	 *
+	 * @return The color associated with this object. May be null if none was set.
+	 * @see #getRegex()
 	 */
 	@Contract(pure = true)
 	@Nullable
@@ -54,11 +65,13 @@ final class Expression {
 	}
 	
 	/**
+	 * Returns a string representation that represents this object.
+	 *
 	 * @return A string representation of this object's regular expression and its associated color.
 	 */
 	@NotNull
 	@Override
 	public String toString() {
-		return String.format("\"%s\" - %s", regex, colorToString(color));
+		return String.format("[regex: \"%s\"; color: %s]", regex, colorToString(color));
 	}
 }
