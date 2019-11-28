ff36afc85ea9a87c1fa8aa7daebaab59a0beae8e
Marc Speckmann <marc.speckmann@gmail.com>
2019-11-28 16:12:11 +0100

    PDF2-103 #comment added first test

--- solution/src/test/java/de/uni_hannover/se/pdfzensor/config/CLErrorMessageHandlerTest.java
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/config/CLErrorMessageHandlerTest.java
@@ -1,7 +1,11 @@
 package de.uni_hannover.se.pdfzensor.config;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import picocli.CommandLine;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,11 +15,39 @@ import static org.junit.jupiter.api.Assertions.*;
 class CLErrorMessageHandlerTest {
 	
 	/**
+	 * Testing if the constructor of {@link CLErrorMessageHandler} is working.
+	 */
+	@Test
+	@DisplayName("Test CLErrorMessageHandler constructor")
+	void testConstructor() {
+		assertNotNull(new CLErrorMessageHandler());
+		assertDoesNotThrow(() -> new CLErrorMessageHandler());
+	}
+	
+	/**
 	 * Testing if {@link CommandLine.ParameterException} is handled the right way.
 	 */
 	@Test
-	void handleParseException() {
-		//TODO:Add tests
-		fail("Test not implemented yet");
+	@DisplayName("Test CLErrorMessageHandler output")
+	void testhandleParseException() {
+		//initialize handler
+		final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+		final PrintStream originalOut = System.err;
+		System.setErr(new PrintStream(outContent));
+		
+		CLErrorMessageHandler handler = new CLErrorMessageHandler();
+		CommandLine cmd = new CommandLine(CLArgs.class);
+		
+		
+		CommandLine.ParameterException ex = new CommandLine.ParameterException(cmd, "Error");
+		handler.handleParseException(ex, new String[]{});
+		
+		assertTrue(outContent.toString()
+							 .contains("Error"));
+		assertTrue(outContent.toString()
+							 .contains(cmd.getHelp()
+										  .fullSynopsis()));
+		
+		System.setOut(originalOut);
 	}
 }
