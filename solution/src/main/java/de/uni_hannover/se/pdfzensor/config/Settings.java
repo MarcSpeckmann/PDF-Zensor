4d1709b39cf54029e0edf1893d6075142fe1eaf0
Tim Hagen <timhagenmail@googlemail.com>
2019-11-29 08:54:29 +0100

    added some more documentation to Settings

--- solution/src/main/java/de/uni_hannover/se/pdfzensor/config/Settings.java
+++ solution/src/main/java/de/uni_hannover/se/pdfzensor/config/Settings.java
@@ -19,39 +19,57 @@ import java.util.Optional;
 import static de.uni_hannover.se.pdfzensor.utils.Utils.colorToString;
 
 /**
- * The class which holds all the settings from the parsed configuration file and commandline arguments, validated and
- * prioritized correctly.
+ * The Settings class constitutes an abstraction and unification of the configuration file ({@link Config}) and the
+ * command line arguments ({@link CLArgs}). Instead of accessing each configuration entity separately, they should be
+ * unified via settings and passed to the outside from there. Upon construction settings passes the command line
+ * arguments to {@link CLArgs}, loads the corresponding configuration file via {@link Config} and takes their parameters
+ * according to the following rules:<br>
+ * <ol>
+ *     <li><b>CLArgs</b> overrides <b>Config</b> overrides <b>Default values</b><br></li>
+ *     <li>No value will be set to null (for this purpose the default values exist)<br></li>
+ *     <li><b>input</b> may only be specified in the CLArgs</li>
+ * </ol>
  */
 public final class Settings {
+	/** The color that text should be censored in if it does not match any other specified expression. */
 	private static final String DEFAULT_CENSOR_COLOR = "#000000";
+	/** The color links should be censored in if nothing else was specified. */
+	private static final Color DEFAULT_LINK_COLOR = Color.BLUE;
 	
+	/** The path at which the pdf-file that should be censored is located. */
 	@NotNull
 	private final File input;
+	/** The path into which the censored pdf-file should be written. */
 	@NotNull
 	private final File output;
+	/** The color with which to censor links. */
 	@NotNull
 	private final Color linkColor;
+	/**
+	 * A set of regex-color-tuples to identify with what color to censor which text. Should at least contain the tuple
+	 * (".", {@link #DEFAULT_CENSOR_COLOR}).
+	 */
 	@NotNull
 	private final Expression[] expressions;
 	
 	/**
-	 * Constructs the settings object from the configuration file and the commandline arguments. The settings of this
-	 * object will be logged at {@link Level#DEBUG}.
+	 * Constructs the settings object from the configuration file and the commandline arguments.
 	 *
 	 * @param configPath the path to the config file (SHOULD BE REMOVED LATER)
 	 * @param args       The commandline arguments.
 	 * @throws IOException If the configuration file could not be parsed.
 	 */
 	public Settings(@NotNull String configPath, @NotNull final String... args) throws IOException {
-		final var clargs = CLArgs.fromStringArray(args);
+		final var clArgs = CLArgs.fromStringArray(args);
 		final var config = getDefaultConfig(configPath);
 		final var configParser = Config.fromFile(config);
-		final var verbose = ObjectUtils.firstNonNull(clargs.getVerbosity(), configParser.getVerbosity(), Level.OFF);
+		final var verbose = ObjectUtils.firstNonNull(clArgs.getVerbosity(), configParser.getVerbosity(), Level.OFF);
 		Logging.init(verbose);
 		
-		input = clargs.getInput();
-		output = checkOutput(ObjectUtils.firstNonNull(clargs.getOutput(), configParser.getOutput(), input.getParentFile()));
-		linkColor = Color.BLUE;
+		input = clArgs.getInput();
+		output = checkOutput(
+				ObjectUtils.firstNonNull(clArgs.getOutput(), configParser.getOutput(), input.getParentFile()));
+		linkColor = DEFAULT_LINK_COLOR;
 		expressions = new Expression[]{new Expression(".", DEFAULT_CENSOR_COLOR)};
 		
 		//Dump to log
@@ -62,7 +80,7 @@ public final class Settings {
 		logger.log(Level.DEBUG, "\tOutput-file: {}", output);
 		logger.log(Level.DEBUG, "\tLogger verbosity: {}", verbose);
 		logger.log(Level.DEBUG, "\tLink-Color: {}", () -> colorToString(linkColor));
-		logger.log(Level.DEBUG, "\tDefined-Expressions");
+		logger.log(Level.DEBUG, "\tExpressions");
 		for (var exp : expressions)
 			logger.log(Level.DEBUG, "\t\t{}", exp);
 	}
@@ -92,8 +110,13 @@ public final class Settings {
 	}
 	
 	/**
-	 * @param out The output file which will be validated.
-	 * @return null if the output to check was null or was invalid, a valid output file otherwise.
+	 * Validates the provided output file. If it is a file it itself will be returned. If it is a folder (or does not
+	 * exist and has no suffix) a path to {@code {out}/{input name}_cens.pdf} is returned.
+	 *
+	 * @param out The output file that should be validated. May not be null.
+	 * @return the validated output file the censored PDF should be written into.
+	 * @throws NullPointerException if out is null
+	 * @see #getDefaultOutput(String)
 	 */
 	@NotNull
 	private File checkOutput(@NotNull final File out) {
