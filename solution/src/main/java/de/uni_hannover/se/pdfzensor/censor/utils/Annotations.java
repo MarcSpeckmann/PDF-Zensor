d62c6958ac9adfaa82f2a6139c254fb54f36fc45
Daniel <brandt_daniel@gmx.de>
2019-11-29 19:33:01 +0100

    PDF2-94 added some throws and added some junit tests

--- solution/src/main/java/de/uni_hannover/se/pdfzensor/censor/utils/Annotations.java
+++ solution/src/main/java/de/uni_hannover/se/pdfzensor/censor/utils/Annotations.java
@@ -47,8 +47,9 @@ public final class Annotations {
 	 * and {@link #cacheHighlights}.
 	 *
 	 * @param page the current PDF page being worked on
+	 * @throws NullPointerException if PDF page is null
 	 */
-	public void cachePage(@NotNull PDPage page) {
+	public void cachePage(@NotNull PDPage page) throws  NullPointerException{
 		cacheLinks(page);
 		cacheHighlights(page);
 	}
@@ -60,7 +61,7 @@ public final class Annotations {
 	 * @param page the current PDF page being worked on
 	 * @throws NullPointerException if given page object is null
 	 */
-	private void cacheLinks(@NotNull PDPage page){
+	private void cacheLinks(@NotNull PDPage page) throws NullPointerException{
 		Objects.requireNonNull(page);
 		try {
 			links = page.getAnnotations(PDAnnotationLink.class::isInstance).stream()
@@ -76,8 +77,9 @@ public final class Annotations {
 	 * in the stream {@link #highlights} list will just be cleared.
 	 *
 	 * @param page the current PDF page being worked on
+	 * @throws NullPointerException if given page object is null
 	 */
-	private void cacheHighlights(@NotNull PDPage page) {
+	private void cacheHighlights(@NotNull PDPage page) throws NullPointerException{
 		Objects.requireNonNull(page);
 		try {
 			highlights = page.getAnnotations(Annotations::isHighlightAnnotation).stream()
@@ -94,8 +96,9 @@ public final class Annotations {
 	 *
 	 * @param rect the rectangle to be checked
 	 * @return true if the given rect entirely fits into at least one rect of {@link #highlights} otherwise false
+	 * @throws NullPointerException if given rect is null
 	 */
-	public boolean isMarked(@NotNull Rectangle2D rect) {
+	public boolean isMarked(@NotNull Rectangle2D rect) throws NullPointerException{
 		return isMarked(rect, MarkCriteria.CONTAIN);
 	}
 
@@ -107,8 +110,9 @@ public final class Annotations {
 	 * @param criteria either {@link MarkCriteria#CONTAIN} or {@link MarkCriteria#INTERSECT}
 	 * @return true if at least one rect of {@link #highlights} matches the criteria with the given
 	 * 			rect otherwise false
+	 * @throws NullPointerException if given rect or criteria is null
 	 */
-	boolean isMarked(@NotNull Rectangle2D rect, @NotNull MarkCriteria criteria) {
+	boolean isMarked(@NotNull Rectangle2D rect, @NotNull MarkCriteria criteria) throws NullPointerException{
 		Objects.requireNonNull(rect);
 		Objects.requireNonNull(criteria);
 		Predicate<Rectangle2D> predicate = criteria.getPredicate(rect);
@@ -121,8 +125,9 @@ public final class Annotations {
 	 *
 	 * @param rect the rectangle to be checked
 	 * @return true if the given rect entirely fits into at least one rect of {@link #links} otherwise false
+	 * @throws NullPointerException if given rect is null
 	 */
-	boolean isLinked(@NotNull Rectangle2D rect) {
+	boolean isLinked(@NotNull Rectangle2D rect) throws NullPointerException{
 		return isLinked(rect, MarkCriteria.CONTAIN);
 	}
 
@@ -134,8 +139,9 @@ public final class Annotations {
 	 * @param criteria either {@link MarkCriteria#CONTAIN} or {@link MarkCriteria#INTERSECT}
 	 * @return true if at least one rect of {@link #links} matches the criteria with the given
 	 * 			rect otherwise false
+	 * @throws NullPointerException if given rect or criteria is null
 	 */
-	boolean isLinked(@NotNull Rectangle2D rect, @NotNull MarkCriteria criteria) {
+	boolean isLinked(@NotNull Rectangle2D rect, @NotNull MarkCriteria criteria) throws NullPointerException{
 		Objects.requireNonNull(rect);
 		Objects.requireNonNull(criteria);
 		Predicate<Rectangle2D> predicate = criteria.getPredicate(rect);
--- solution/src/test/java/de/uni_hannover/se/pdfzensor/censor/utils/AnnotationsTest.java
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/censor/utils/AnnotationsTest.java
@@ -1,8 +1,52 @@
 package de.uni_hannover.se.pdfzensor.censor.utils;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import de.uni_hannover.se.pdfzensor.censor.utils.*;
+
+import java.awt.geom.Rectangle2D;
+
 /** AnnotationsTest should contain all unit-tests related to {@link Annotations}. */
 public class AnnotationsTest {
 
+    /** tests for {@link Annotations} constructor */
+    @Test
+    void testAnnotationsConstructor(){
+        Assertions.assertDoesNotThrow(AnnotationsTest::new);
+    }
 
+    /** test for {@link Annotations#cachePage} */
+    @Test
+    void testCachePage(){
+        Assertions.assertThrows(NullPointerException.class, () -> new Annotations().cachePage(null));
+    }
 
+    /** test for {@link Annotations#isMarked(Rectangle2D, MarkCriteria)} */
+    @Test
+    void testIsMarked(){
+        // both arguments are null
+        Assertions.assertThrows(NullPointerException.class, () -> new Annotations().isMarked(null, null));
+    }
+
+    /** test for {@link Annotations#isMarked(Rectangle2D)} */
+    @Test
+    void testIsMarkedOnlyContain(){
+        Assertions.assertThrows(NullPointerException.class, () -> new Annotations().isMarked(null));
+    }
+
+    /** test for {@link Annotations#isLinked(Rectangle2D, MarkCriteria)} */
+    @Test
+    void testIsLinked(){
+        // both arguments are null
+        Assertions.assertThrows(NullPointerException.class, () -> new Annotations().isLinked(null, null));
+    }
+
+    /** test for {@link Annotations#isLinked(Rectangle2D)} */
+    @Test
+    void testIsLinkedOnlyContain(){
+        Assertions.assertThrows(NullPointerException.class, () -> new Annotations().isLinked(null));
+    }
 }
