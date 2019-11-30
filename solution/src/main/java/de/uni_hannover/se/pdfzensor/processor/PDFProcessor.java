28e21a85a5b5357ef8deaae5e52722ef896ccd29
Lennart Möller <moeller.lennart@gmail.com>
2019-11-30 16:58:42 +0100

    #PDF2-92 #comment JavaDoc for PDFProcessor

--- solution/src/main/java/de/uni_hannover/se/pdfzensor/processor/PDFProcessor.java
+++ solution/src/main/java/de/uni_hannover/se/pdfzensor/processor/PDFProcessor.java
@@ -5,12 +5,24 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import java.io.IOException;
 import java.util.Objects;
 
+/**
+ * PDFProcessor is a class that runs a TextProcessor with a PDFHandler
+ */
 public class PDFProcessor {
     private final PDFHandler handler;
-
+    
+    /**
+     * @param handler The PDFHandler that is used for the TextProcessor to open and close the document and pages
+     */
     public PDFProcessor(PDFHandler handler) {
         this.handler = Objects.requireNonNull(handler, "PDFHandler must not be null");
     }
+    
+    /**
+     * This function runs the TextProcessor with the delivered PDF document
+     * @param document The document to be processed in the TextProcessor
+     * @throws IOException The exception is thrown in the constructor of the TextProcessor
+     */
     public void process(PDDocument document) throws IOException {
         final PDFStreamProcessor processor = new TextProcessor(handler);
         processor.getText(document);
