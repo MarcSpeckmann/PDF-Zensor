9ce6ca047f962aadc102f3eb189cddece383032b
Lennart Möller <moeller.lennart@gmail.com>
2019-12-01 20:21:37 +0100

    #PDF2-93 #comment reformat code

--- solution/src/main/java/de/uni_hannover/se/pdfzensor/censor/PDFCensor.java
+++ solution/src/main/java/de/uni_hannover/se/pdfzensor/censor/PDFCensor.java
@@ -17,66 +17,66 @@ import de.uni_hannover.se.pdfzensor.config.Settings;
  * annotations and the mode
  */
 public final class PDFCensor implements PDFHandler {
-    private Predicate<Rectangle2D> removePredicate;
-    
-    /**
-     * @param settings Settings that contain information about the mode and expressions
-     * @throws NullPointerException if settings is null
-     */
-    public PDFCensor(@NotNull Settings settings) throws NullPointerException {
-        Objects.requireNonNull(settings);
-        this.removePredicate = rect -> true;
-    }
-    
-    /**
-     * A callback for when work on a new document starts.
-     *
-     * @param doc the document which is being worked on
-     */
-    @Override
-    public void beginDocument(PDDocument doc) {
-
-    }
-    
-    /**
-     * A callback for when work on a new page starts.
-     *
-     * @param doc     the document which is being worked on
-     * @param page    the PDPage (current pdf page) that is being worked on
-     * @param pageNum the number of the page that is being worked on
-     */
-    @Override
-    public void beginPage(PDDocument doc, PDPage page, int pageNum) {
-
-    }
-    
-    /**
-     * A callback for when work on a page has ended.
-     *
-     * @param doc     the document which is being worked on
-     * @param page    the PDPage (current pdf page) that is being worked on
-     * @param pageNum the number of the page that is being worked on
-     */
-    @Override
-    public void endPage(PDDocument doc, PDPage page, int pageNum) {
-
-    }
-    
-    /**
-     * A callback for when work on a document has ended.
-     *
-     * @param doc the document which is being worked on
-     */
-    @Override
-    public void endDocument(PDDocument doc) {
-    }
-    
-    /**
-     * @param pos the TextPosition (represents string + its character's screen positions) to check
-     * @return true if {@code pos} should be censored, false otherwise
-     */
-    @Override
-    public boolean shouldCensorText(TextPosition pos){
-        return true;
-    }
+	private Predicate<Rectangle2D> removePredicate;
+	
+	/**
+	 * @param settings Settings that contain information about the mode and expressions
+	 * @throws NullPointerException if settings is null
+	 */
+	public PDFCensor(@NotNull Settings settings) throws NullPointerException {
+		Objects.requireNonNull(settings);
+		this.removePredicate = rect -> true;
+	}
+	
+	/**
+	 * A callback for when work on a new document starts.
+	 *
+	 * @param doc the document which is being worked on
+	 */
+	@Override
+	public void beginDocument(PDDocument doc) {
+	
+	}
+	
+	/**
+	 * A callback for when work on a new page starts.
+	 *
+	 * @param doc     the document which is being worked on
+	 * @param page    the PDPage (current pdf page) that is being worked on
+	 * @param pageNum the number of the page that is being worked on
+	 */
+	@Override
+	public void beginPage(PDDocument doc, PDPage page, int pageNum) {
+	
+	}
+	
+	/**
+	 * A callback for when work on a page has ended.
+	 *
+	 * @param doc     the document which is being worked on
+	 * @param page    the PDPage (current pdf page) that is being worked on
+	 * @param pageNum the number of the page that is being worked on
+	 */
+	@Override
+	public void endPage(PDDocument doc, PDPage page, int pageNum) {
+	
+	}
+	
+	/**
+	 * A callback for when work on a document has ended.
+	 *
+	 * @param doc the document which is being worked on
+	 */
+	@Override
+	public void endDocument(PDDocument doc) {
+	}
+	
+	/**
+	 * @param pos the TextPosition (represents string + its character's screen positions) to check
+	 * @return true if {@code pos} should be censored, false otherwise
+	 */
+	@Override
+	public boolean shouldCensorText(TextPosition pos) {
+		return true;
+	}
 }
