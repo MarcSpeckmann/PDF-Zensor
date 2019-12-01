dac7a4903dba695b14eb5e868eca2949bdb1119e
Lennart Möller <moeller.lennart@gmail.com>
2019-12-01 20:20:03 +0100

    #PDF2-92 #comment reformat code

--- solution/src/test/java/de/uni_hannover/se/pdfzensor/processor/PDFProcessorTest.java
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/processor/PDFProcessorTest.java
@@ -19,76 +19,84 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-/** PDFProcessorTest should contain all unit-tests related to {@link PDFProcessor}. */
+/**
+ * PDFProcessorTest should contain all unit-tests related to {@link PDFProcessor}.
+ */
 class PDFProcessorTest {
-	/** Path to the pdf-tests Resources */
+	/**
+	 * Path to the pdf-tests Resources
+	 */
 	private static final String PDF_PATH = "/pdf-files/";
-
+	
 	/**
 	 * a dummy initialization of the {@link #dummyHandler} that we need to test if a pdf-document is being
 	 * processed In the right order, also to check if the functions were called at all,
 	 * its remove all the pages of the processed document so can we be sure that its been processed by checking
 	 * if the document is empty
-	 * */
+	 */
 	PDFHandler dummyHandler = new PDFHandler() {
 		@Override
 		public void beginDocument(final PDDocument doc) {
 			System.out.println("start processing..");
 			System.out.println("beginDocument");
 		}
+		
 		@Override
 		public void beginPage(final PDDocument doc, final PDPage page, final int pageNum) {
 			System.out.println("beginPage");
 			doc.removePage(page);
 		}
+		
 		@Override
 		public void endPage(final PDDocument doc, final PDPage page, final int pageNum) {
 			System.out.println("EndPage");
 		}
+		
 		@Override
 		public void endDocument(final PDDocument doc) {
 			System.out.println("EndDocument");
 		}
+		
 		@Override
 		public boolean shouldCensorText(final TextPosition pos) {
 			return false;
 		}
 	};
-
+	
 	/**
 	 * tests the {@link PDFProcessor#PDFProcessor(PDFHandler)} by valid and invalid input
 	 */
 	@Test
-	void testConstructor()  {
+	void testConstructor() {
 		assertThrows(NullPointerException.class, () -> new PDFProcessor(null));
 		assertDoesNotThrow(() -> new PDFProcessor(dummyHandler));
 	}
-
+	
 	/**
 	 * @return A stream of all files in the directory {@link #PDF_PATH}.
 	 * @throws IOException If there is an error loading the files.
 	 */
 	private static Stream<Arguments> testForFile() throws IOException {
-		return Files.walk(Paths.get(TestUtility.getResource(PDF_PATH).getAbsolutePath())).map(Path::toFile)
-				.filter(File::isFile).map(Arguments::of);
+		return Files.walk(Paths.get(TestUtility.getResource(PDF_PATH).getAbsolutePath())).map(Path::toFile).filter(File::isFile).map(Arguments::of);
 	}
-
+	
 	/**
-	 *  checks if a pdf-document is being processed In the right order, also it checks
-	 *  if the functions were called at all
+	 * checks if a pdf-document is being processed In the right order, also it checks
+	 * if the functions were called at all
+	 *
 	 * @param file The PDF File that is being processed.
 	 * @throws IOException if an IO error occurs.
 	 */
 	@ParameterizedTest(name = "Run {index}: file: {0}")
 	@MethodSource("testForFile")
 	void process(File file) throws IOException {
-
+		
 		var doc = PDDocument.load(file);
 		PDFProcessor processor = new PDFProcessor(dummyHandler);
 		System.out.println("pageCounts: " + doc.getPages().getCount());
 		processor.process(doc);
-
+		
 		// it should be always 0 because the dummy handler removes all pages
-		assertEquals(0,doc.getPages().getCount());
+		assertEquals(0, doc.getPages().getCount());
 	}
 }
