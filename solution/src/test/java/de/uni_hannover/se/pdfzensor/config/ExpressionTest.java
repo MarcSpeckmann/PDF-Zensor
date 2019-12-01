3daedcfd35cf2a632ec830442493eaf975ddb683
ahmad <ahmadramadan@gmx.de>
2019-12-01 21:43:26 +0100

    #PDF2-101 #comment added java-doc to ExpressionTest

--- solution/src/test/java/de/uni_hannover/se/pdfzensor/config/ExpressionTest.java
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/config/ExpressionTest.java
@@ -12,21 +12,25 @@ import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/** CLArgsTest should contain all unit-tests related to {@link CLArgs}. */
 class ExpressionTest {
-	
+
+	/** Checks if the constructor throws a <code>NullPointerException</code> if the input-regex is null.*/
 	@SuppressWarnings("ConstantConditions")
 	@Test
 	void testConstructor() {
 		assertThrows(NullPointerException.class, () -> new Expression(null, "#ffffff"));
 	}
-	
+
+	/** Checks if the constructor can handle the initialization when the color is <code>null</code>.*/
 	@Test
 	void testNullColor() {
 		var nullColor = new Expression("regex", null);
 		assertEquals("regex", nullColor.getRegex());
 		assertNull(nullColor.getColor());
 	}
-	
+
+	/** Checks if the constructor can handle the initialization when the color is valid.*/
 	@ParameterizedTest
 	@ArgumentsSource(ColorProvider.class)
 	void testValidColor(@NotNull String[] colorCodes, Color color) {
@@ -36,7 +40,8 @@ class ExpressionTest {
 		assertEquals(Utils.getColorOrNull(colorCode), exp.getColor());
 		assertDoesNotThrow(exp::toString);
 	}
-	
+
+	/** Checks if the constructor can handle the initialization when the color is invalid.*/
 	//More rigorous testing for this is done in UtilsTest
 	@ParameterizedTest
 	@ValueSource(strings = {"#", "ffffff", "fffffz"})
