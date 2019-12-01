84e085bb65d01579b498d012db76820b4ea07c0d
ahmad <ahmadramadan@gmx.de>
2019-12-01 22:36:40 +0100

    #PDF2-92 #comment changed the way to test if the PDFhandler are called in the right order

--- solution/src/test/java/de/uni_hannover/se/pdfzensor/processor/PDFProcessorTest.java
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/processor/PDFProcessorTest.java
@@ -23,9 +23,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class PDFProcessorTest {
 	/** Path to the pdf-tests Resources */
 	private static final String PDF_PATH = "/pdf-files/";
+	
+	/**to checks a pdf-document is being processed In the right order */
+	private int counter = 0;
+	/**to checks a pdf-document is being closed and opened In the right order */
+	private boolean isStarted = false;
 
 	/**
-	 * a dummy initialization of the {@link #dummyHandler} that we need to test if a pdf-document is being
+	 * a dummy initialization of the that we need to test if a pdf-document is being
 	 * processed In the right order, also to check if the functions were called at all,
 	 * its remove all the pages of the processed document so can we be sure that its been processed by checking
 	 * if the document is empty
@@ -33,21 +38,38 @@ class PDFProcessorTest {
 	PDFHandler dummyHandler = new PDFHandler() {
 		@Override
 		public void beginDocument(final PDDocument doc) {
-			System.out.println("start processing..");
-			System.out.println("beginDocument");
+			// it should not be opened by now
+			assertFalse(isStarted);
+			// mark it as open
+			isStarted = true;
 		}
 		@Override
 		public void beginPage(final PDDocument doc, final PDPage page, final int pageNum) {
-			System.out.println("beginPage");
+			//should be true as the document is opened
+			assertTrue(isStarted);
+			//it should not call beginPage before it end the last page
+			assertEquals(0,counter);
+			// increments it by opening a new page
+			counter += 1;
+			// to checks if the function is called at all
 			doc.removePage(page);
 		}
 		@Override
 		public void endPage(final PDDocument doc, final PDPage page, final int pageNum) {
-			System.out.println("EndPage");
+			//should be true as the document is opened
+			assertTrue(isStarted);
+			//it should not call endPage before it begin the page
+			assertEquals(1,counter);
+			// decrements it by ending the page
+			counter -= 1;
 		}
 		@Override
 		public void endDocument(final PDDocument doc) {
-			System.out.println("EndDocument");
+			// there are no pages that es being not closed
+			assertEquals(0,counter);
+			//should be true as the document is opened
+			assertTrue(isStarted);
+			isStarted = false;
 		}
 		@Override
 		public boolean shouldCensorText(final TextPosition pos) {
@@ -90,5 +112,7 @@ class PDFProcessorTest {
 
 		// it should be always 0 because the dummy handler removes all pages
 		assertEquals(0,doc.getPages().getCount());
+		// cause the document must be closed after all
+		assertFalse(isStarted);
 	}
 }
