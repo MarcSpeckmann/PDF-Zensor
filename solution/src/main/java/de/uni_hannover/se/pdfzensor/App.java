d6dd03f027f7c03e5725aaf4b495f52409138438
Marc Speckmann <marc.speckmann@gmail.com>
2019-12-03 14:57:51 +0100

    extended main to call PDFCensor

--- solution/src/main/java/de/uni_hannover/se/pdfzensor/App.java
+++ solution/src/main/java/de/uni_hannover/se/pdfzensor/App.java
@@ -1,9 +1,12 @@
 package de.uni_hannover.se.pdfzensor;
 
 
+import de.uni_hannover.se.pdfzensor.censor.PDFCensor;
 import de.uni_hannover.se.pdfzensor.config.CLErrorMessageHandler;
 import de.uni_hannover.se.pdfzensor.config.CLHelp;
 import de.uni_hannover.se.pdfzensor.config.Settings;
+import de.uni_hannover.se.pdfzensor.processor.PDFProcessor;
+import org.apache.pdfbox.pdmodel.PDDocument;
 import picocli.CommandLine;
 
 import java.io.IOException;
@@ -14,7 +17,13 @@ public class App {
 		try {
 			if (!CLHelp.printStandardHelpOptionsIfRequested(args)) {
 				
-				Settings settings = new Settings("", args);
+				final var settings = new Settings(null, args);
+				final var censor = new PDFCensor(settings);
+				final var processor = new PDFProcessor(censor);
+				final var doc = PDDocument.load(settings.getInput());
+				processor.process(doc);
+				doc.save(settings.getOutput());
+				
 				
 			}
 		} catch (CommandLine.ParameterException ex) {
