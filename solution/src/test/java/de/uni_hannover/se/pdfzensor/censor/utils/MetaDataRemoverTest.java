32d1f68998a972d155c1cf77ffc9e20a08faaf61
Tim Hagen <timhagenmail@googlemail.com>
2019-12-04 12:06:10 +0100

    Refactored the MetaDataRemoverTest

--- solution/src/test/java/de/uni_hannover/se/pdfzensor/censor/utils/MetaDataRemoverTest.java
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/censor/utils/MetaDataRemoverTest.java
@@ -1,49 +1,46 @@
 package de.uni_hannover.se.pdfzensor.censor.utils;
 
+import de.uni_hannover.se.pdfzensor.TestUtility;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.pdfbox.pdmodel.PDDocument;
-import org.apache.pdfbox.pdmodel.PDDocumentInformation;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-import java.io.File;
+
 import java.io.IOException;
-import java.util.Calendar;
 
-//TODO add more tests and maybe alter the structure of the tests
+import static org.junit.jupiter.api.Assertions.*;
+
+//TODO add more tests and maybe alter the structure of the tests (one test may be to check an entire file for the metadata)
 class MetaDataRemoverTest {
-    /**
-     * Tests if all of the metadata have been censored correctly using a valid PDF.
-     * @throws IOException if an IO error occurs.
-     */
-    @Test
-    void testCensorMetadataValidFile() throws IOException {
-        String path = "src/test/resources/pdf-files/fullMetadata.pdf";
-
-        File file = new File(path);
-        PDDocument document = PDDocument.load(file);
-        PDDocumentInformation docInfo = document.getDocumentInformation();
-
-        MetadataRemover.censorMetadata(document);
-
-        docInfo = document.getDocumentInformation();
-        assertTrue(StringUtils.isEmpty(docInfo.getAuthor()));
-        assertTrue(StringUtils.isEmpty(docInfo.getCreator()));
-        assertTrue(StringUtils.isEmpty(docInfo.getProducer()));
-        assertTrue(StringUtils.isEmpty(docInfo.getTitle()));
-        assertTrue(StringUtils.isEmpty(docInfo.getSubject()));
-        assertTrue(StringUtils.isEmpty(docInfo.getKeywords()));
-        assertNull(docInfo.getCreationDate());
-        assertNull(docInfo.getModificationDate());
-        document.close();
-    }
-
-    /**
-     * Tests if the MetadateRemover does not throw an exception if it is unable
-     * to retrieve the document information of the given file
-     */
-    @SuppressWarnings("ConstantConditions")
-    @Test
-    void testCensorMetadataInvalidFile(){
-        assertThrows(NullPointerException.class, () -> MetadataRemover.censorMetadata(null));
-    }
+	/**
+	 * Tests if all of the metadata have been censored correctly using a valid PDF.
+	 *
+	 * @throws IOException if an IO error occurs.
+	 */
+	@Test
+	void testCensorMetadataValidFile() throws IOException {
+		var file = TestUtility.getResource("/pdf-files/fullMetadata.pdf");
+		try (final var document = PDDocument.load(file)) {
+			MetadataRemover.censorMetadata(document);
+			
+			var docInfo = document.getDocumentInformation();
+			assertTrue(StringUtils.isEmpty(docInfo.getAuthor()));
+			assertTrue(StringUtils.isEmpty(docInfo.getCreator()));
+			assertTrue(StringUtils.isEmpty(docInfo.getProducer()));
+			assertTrue(StringUtils.isEmpty(docInfo.getTitle()));
+			assertTrue(StringUtils.isEmpty(docInfo.getSubject()));
+			assertTrue(StringUtils.isEmpty(docInfo.getKeywords()));
+			assertNull(docInfo.getCreationDate());
+			assertNull(docInfo.getModificationDate());
+		}
+	}
+	
+	/**
+	 * Tests if the MetadateRemover does not throw an exception if it is unable to retrieve the document information of
+	 * the given file
+	 */
+	@SuppressWarnings("ConstantConditions")
+	@Test
+	void testCensorMetadataInvalidFile() {
+		assertThrows(NullPointerException.class, () -> MetadataRemover.censorMetadata(null));
+	}
 }
