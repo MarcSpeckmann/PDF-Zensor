fe8e2674b05f8d3c31100d79ca3fd51abb9a5ce1
Lennart Möller <moeller.lennart@gmail.com>
2019-12-04 10:06:23 +0100

    #PDF2-94 #comment WIP: unable to initialize PDType3Font

--- solution/src/test/java/de/uni_hannover/se/pdfzensor/censor/utils/PDFUtilsTest.java
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/censor/utils/PDFUtilsTest.java
@@ -1,7 +1,9 @@
 package de.uni_hannover.se.pdfzensor.censor.utils;
 
 import de.uni_hannover.se.pdfzensor.TestUtility;
+import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import org.apache.pdfbox.pdmodel.font.PDType3Font;
 import org.apache.pdfbox.text.TextPosition;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
 
@@ -25,6 +27,10 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class PDFUtilsTest {
 	
+	/**
+	 * PDType3Font for testing
+	 */
+	private static PDType3Font pdType3Font;
 	
 	/**
 	 * New data structure TextPositionValue to bundle the values for transformTextPosition
@@ -39,8 +45,9 @@ class PDFUtilsTest {
 		float spaceWidth;
 		String unicode;
 		int[] charCodes;
+		PDFont font;
 		
-		TextPositionValue(float endX, float endY, float fontSize, int fontSizeInPt, float maxHeight, float individualWidth, float spaceWidth, String unicode, int[] charCodes) {
+		TextPositionValue(float endX, float endY, float fontSize, int fontSizeInPt, float maxHeight, float individualWidth, float spaceWidth, String unicode, int[] charCodes, PDFont font) {
 			this.endX = endX;
 			this.endY = endY;
 			this.fontSize = fontSize;
@@ -50,6 +57,7 @@ class PDFUtilsTest {
 			this.spaceWidth = spaceWidth;
 			this.unicode = unicode;
 			this.charCodes = charCodes;
+			this.font = font;
 		}
 	}
 	
@@ -58,13 +66,19 @@ class PDFUtilsTest {
 	 */
 	private static final Map<Rectangle2D, TextPositionValue> TEXTPOSITION = new HashMap<>();
 	
-	private static TextPositionValue tpValue1 = new TextPositionValue(79.19946f, 800.769f, 10.9091f, 10, 7.51637f, 8.333466f, 3.0545478f, "D", new int[]{68});
-	private static TextPositionValue tpValue2 = new TextPositionValue(23.1547f, 44.32212f, 11.95f, 11, 7.51637f, 8.333466f, 3.0545478f, "DE", new int[]{68, 69});
+	private static TextPositionValue tpValue1 = new TextPositionValue(79.19946f, 800.769f, 10.9091f, 10, 7.51637f, 8.333466f, 3.0545478f, "D", new int[]{68}, PDType1Font.TIMES_ROMAN);
+	private static TextPositionValue tpValue2 = new TextPositionValue(23.1547f, 44.32212f, 11.95f, 11, 7.51637f, 8.333466f, 3.0545478f, "DE", new int[]{68, 69}, PDType1Font.TIMES_ROMAN);
+	// TODO: Initialize PDType3Font...
+	private static TextPositionValue tpValue3 = new TextPositionValue(79.19946f, 800.769f, 10.9091f, 10, 7.51637f, 8.333466f, 3.0545478f, "D", new int[]{68}, pdType3Font);
 	
 	static {
+		// test with one char in TextPosition
 		TEXTPOSITION.put(new Rectangle2D.Float(tpValue1.endX, tpValue1.endY, 7.876370270042557f, 9.796371887116607f), tpValue1);
+		// test with two chars in TextPosition
 		TEXTPOSITION.put(new Rectangle2D.Float(tpValue2.endX, tpValue2.endY, 15.929350502353941f, 10.731100338419985f), tpValue2);
-		// TODO: tests for: empty position and PDType3Font
+		// test with PDType3Font
+		// TEXTPOSITION.put(new Rectangle2D.Float(tpValue3.endX, tpValue3.endY, 7.876370270042557f, 9.796371887116607f), tpValue3);
+		// TODO: not working yet because I can't initialize a PDType3Font
 	}
 	
 	
@@ -83,9 +97,8 @@ class PDFUtilsTest {
 	@MethodSource("textPositionProvider")
 	void transformTextPositionTest(@NotNull TextPositionValue input, @NotNull Rectangle2D expected) {
 		// pageRotation = 0 is a standard value
-		// pageWidth = 595,276f and pageHeight=841,89f are height and width of a DIN-A4-PDF, but is irrelevant for this
-		// task
-		TextPosition tp = new TextPosition(0, 595.276f, 841.89f, new Matrix(input.fontSize, 0f, 0f, input.fontSize, input.endX, input.endY), input.endX, input.endY, input.maxHeight, input.individualWidth, input.spaceWidth, input.unicode, input.charCodes, PDType1Font.TIMES_ROMAN, input.fontSize, input.fontSizeInPt);
+		// pageWidth = 595,276f and pageHeight=841,89f are height and width of a DIN-A4-PDF, but is irrelevant for this test
+		TextPosition tp = new TextPosition(0, 595.276f, 841.89f, new Matrix(input.fontSize, 0f, 0f, input.fontSize, input.endX, input.endY), input.endX, input.endY, input.maxHeight, input.individualWidth, input.spaceWidth, input.unicode, input.charCodes, input.font, input.fontSize, input.fontSizeInPt);
 		try {
 			assertEquals(Math.round(expected.getHeight()), Math.round(Objects.requireNonNull(PDFUtils.transformTextPosition(tp)).getHeight()));
 			assertEquals(Math.round(expected.getWidth()), Math.round(Objects.requireNonNull(PDFUtils.transformTextPosition(tp)).getWidth()));
@@ -136,5 +149,9 @@ class PDFUtilsTest {
 		TestUtility.assertIsUtilityClass(PDFUtils.class);
 		assertThrows(IllegalArgumentException.class, () -> PDFUtils.pdRectToRect2D(null)); // ignore SonarLint because we wan't that bad input
 		assertThrows(IllegalArgumentException.class, () -> PDFUtils.transformTextPosition(null)); // ignore SonarLint because we wan't that bad input
+		
+		Rectangle2D rect = new Rectangle2D.Float(0f, 0f, 0f, 0f);
+		TextPosition tp = new TextPosition(0, 0f, 0f, new Matrix(0f, 0f, 0f, 0f, 0f, 0f), 0f, 0f, 0f, 0f, 0f, "", new int[]{}, null, 0f, 0);
+		assertThrows(NullPointerException.class, () -> PDFUtils.transformTextPosition(tp));
 	}
 }
