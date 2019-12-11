95ee6abeb02626192dc530778a5d103a73dd3819
Tim Hagen <timhagenmail@googlemail.com>
2019-12-11 07:54:18 +0100

    updated tests according to the last commit

--- solution/src/test/java/de/uni_hannover/se/pdfzensor/LoggingTest.java
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/LoggingTest.java
@@ -55,7 +55,7 @@ class LoggingTest {
 		assertFalse(isLoggingInitialized());
 		assertNotNull(Logging.getLogger());
 		assertTrue(isLoggingInitialized());
-		assertEquals(Level.OFF, getRootLogger().orElseThrow().getLevel());
+		assertEquals(Level.ERROR, getRootLogger().orElseThrow().getLevel());
 		Logging.deinit();
 		assertFalse(isLoggingInitialized());
 	}
