cc9f2fd54e3c164610c17ed1cf8472e4339dd7b0
siddhartha <maksim.gluzman@web.de>
2019-12-11 12:13:49 +0100

    PDF2-60 #comment added javadoc

--- solution/src/main/java/de/uni_hannover/se/pdfzensor/images/ImageRemover.java
+++ solution/src/main/java/de/uni_hannover/se/pdfzensor/images/ImageRemover.java
@@ -4,14 +4,28 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.graphics.PDXObject;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
+import javax.naming.OperationNotSupportedException;
 import java.io.IOException;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 
 /**
- * TODO add javaDoc
+ * Utility-class that removes Images ({@link PDImageXObject}) from a given {@link PDDocument}.
  */
 public final class ImageRemover {
+	/**
+	 * Private constructor of a utility-class that is not supposed to be called.
+	 * @throws OperationNotSupportedException when called.
+	 */
+	private ImageRemover() throws OperationNotSupportedException {
+		throw new OperationNotSupportedException("");
+	}
+
+	/**
+	 * Static function that will remove the Images {@link PDImageXObject} from the given {@link PDDocument}.
+	 * @param doc {@link PDDocument} that will have its Images {@link PDImageXObject} removed.
+	 * @throws IOException
+	 */
 	static public void remove(PDDocument doc) throws IOException {
 		var myListMap = new ArrayList<AbstractMap.SimpleEntry<Integer, org.apache.pdfbox.cos.COSName>>();
 		for (var i = 0; i < doc.getNumberOfPages(); ++i) {
