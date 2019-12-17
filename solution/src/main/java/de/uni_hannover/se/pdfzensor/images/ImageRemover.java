6b76e3999a3e346e88860733588d94460fb0d5eb
siddhartha <maksim.gluzman@web.de>
2019-12-17 19:20:07 +0100

    PDF2-60 #comment fix format

--- solution/src/main/java/de/uni_hannover/se/pdfzensor/images/ImageRemover.java
+++ solution/src/main/java/de/uni_hannover/se/pdfzensor/images/ImageRemover.java
@@ -3,11 +3,11 @@ package de.uni_hannover.se.pdfzensor.images;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.graphics.PDXObject;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+
 import javax.naming.OperationNotSupportedException;
 import java.io.IOException;
 import java.util.AbstractMap;
 import java.util.ArrayList;
-import java.util.Objects;
 
 /**
  * Utility-class that removes Images ({@link PDImageXObject}) from a given {@link PDDocument}.
@@ -15,6 +15,7 @@ import java.util.Objects;
 public final class ImageRemover {
 	/**
 	 * Private constructor of a utility-class that is not supposed to be called.
+	 *
 	 * @throws OperationNotSupportedException when called.
 	 */
 	private ImageRemover() throws OperationNotSupportedException {
@@ -23,22 +24,24 @@ public final class ImageRemover {
 
 	/**
 	 * Static function that will remove the Images {@link PDImageXObject} from the given {@link PDDocument}.
+	 *
 	 * @param doc {@link PDDocument} that will have its Images {@link PDImageXObject} removed.
 	 * @throws IOException when there is an error at retrieving the {@link PDXObject}.
 	 */
 	// TODO added try and catch with ignore to discus how to handle this
 	public static void remove(PDDocument doc) throws IOException {
-			var myListMap = new ArrayList<AbstractMap.SimpleEntry<Integer, org.apache.pdfbox.cos.COSName>>();
-			try{
-				for (var i = 0; i < doc.getNumberOfPages(); ++i) {
-					for (var name : doc.getPage(i).getResources().getXObjectNames()) {
-						var xObject = doc.getPage(i).getResources().getXObject(name);
-						if (xObject instanceof PDImageXObject)
-							myListMap.add(new AbstractMap.SimpleEntry<>(i, name));
-					}
+		var myListMap = new ArrayList<AbstractMap.SimpleEntry<Integer, org.apache.pdfbox.cos.COSName>>();
+		try {
+			for (var i = 0; i < doc.getNumberOfPages(); ++i) {
+				for (var name : doc.getPage(i).getResources().getXObjectNames()) {
+					var xObject = doc.getPage(i).getResources().getXObject(name);
+					if (xObject instanceof PDImageXObject)
+						myListMap.add(new AbstractMap.SimpleEntry<>(i, name));
 				}
-				for (var item : myListMap)
-					doc.getPage(item.getKey()).getResources().put(item.getValue(), (PDXObject) null);
-			}catch (NullPointerException ignored){};
+			}
+			for (var item : myListMap)
+				doc.getPage(item.getKey()).getResources().put(item.getValue(), (PDXObject) null);
+		} catch (NullPointerException ignored) {
+		}
 	}
 }
--- solution/src/test/java/de/uni_hannover/se/pdfzensor/images/ImageRemoverTest.java
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/images/ImageRemoverTest.java
@@ -3,10 +3,8 @@ package de.uni_hannover.se.pdfzensor.images;
 import de.uni_hannover.se.pdfzensor.testing.TestUtility;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.File;
@@ -17,44 +15,49 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 import static de.uni_hannover.se.pdfzensor.testing.TestConstants.PDF_RESOURCE_PATH;
-import static de.uni_hannover.se.pdfzensor.testing.TestUtility.getResourcePath;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 //TODO: add further tests
-/** PDFProcessorTest should contain all unit-tests related to {@link ImageRemover}. */
-class ImageRemoverTest {
-    /** Path to the pdf-tests Resources */
-    //private static final String pdfPath = getResourcePath(PDF_RESOURCE_PATH + "threeImages.pdf");
-	@ParameterizedTest
-	@MethodSource("testArguments")
-    void testRemove(File file) throws IOException {
-		PDDocument testDocument = PDDocument.load(file);
-        //assertEquals(3, countPDImageXObjects(testDocument));
-        //
-        ImageRemover.remove(testDocument);
-        //
-        assertEquals(0, countPDImageXObjects(testDocument));
-        testDocument.close();
-    }
 
-    private static Stream<Arguments> testArguments() throws IOException {
-    	return Files.walk(Paths.get(TestUtility
+/**
+ * PDFProcessorTest should contain all unit-tests related to {@link ImageRemover}.
+ */
+class ImageRemoverTest {
+	private static Stream<Arguments> testArguments() throws IOException {
+		return Files.walk(Paths.get(TestUtility
 				.getResource(PDF_RESOURCE_PATH).getAbsolutePath()))
 				.map(Path::toFile)
 				.filter(File::isFile)
 				.map(Arguments::of);
 	}
 
-    /**
-     * Counts the number of {@link PDImageXObject} in the given {@link PDDocument}.
-     * @param document is the {@link PDDocument} that is looked through.
-     * @return the number of {@link PDImageXObject} found.
-     * @throws IOException when there is an Error retrieving the xObjectNames.
-     */
-    private int countPDImageXObjects(PDDocument document) throws IOException {
-        var pdImageObjectCounter = 0;
+	/**
+	 * Path to the pdf-tests Resources
+	 */
+	//private static final String pdfPath = getResourcePath(PDF_RESOURCE_PATH + "threeImages.pdf");
+	@ParameterizedTest
+	@MethodSource("testArguments")
+	void testRemove(File file) throws IOException {
+		PDDocument testDocument = PDDocument.load(file);
+		//assertEquals(3, countPDImageXObjects(testDocument));
+		//
+		ImageRemover.remove(testDocument);
+		//
+		assertEquals(0, countPDImageXObjects(testDocument));
+		testDocument.close();
+	}
+
+	/**
+	 * Counts the number of {@link PDImageXObject} in the given {@link PDDocument}.
+	 *
+	 * @param document is the {@link PDDocument} that is looked through.
+	 * @return the number of {@link PDImageXObject} found.
+	 * @throws IOException when there is an Error retrieving the xObjectNames.
+	 */
+	private int countPDImageXObjects(PDDocument document) throws IOException {
+		var pdImageObjectCounter = 0;
 		// TODO added try and catch with ignore to discus how to handle this
-        try {
+		try {
 			for (var page : document.getPages()) {
 				for (var name : page.getResources().getXObjectNames()) {
 					var xObject = page.getResources().getXObject(name);
@@ -62,8 +65,9 @@ class ImageRemoverTest {
 						pdImageObjectCounter++;
 				}
 			}
-		} catch (NullPointerException ignore){}
-        return pdImageObjectCounter;
-    }
+		} catch (NullPointerException ignore) {
+		}
+		return pdImageObjectCounter;
+	}
 
 }
