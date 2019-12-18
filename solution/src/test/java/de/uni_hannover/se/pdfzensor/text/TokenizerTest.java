f27248b36f80489366497e2f8a66bba5555679f7
Tim Hagen <timhagenmail@googlemail.com>
2019-12-18 17:38:47 +0100

    Implemented validity checks for tokens and test them

--- solution/src/test/java/de/uni_hannover/se/pdfzensor/text/TokenizerTest.java
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/text/TokenizerTest.java
@@ -1,18 +1,25 @@
 package de.uni_hannover.se.pdfzensor.text;
 
 import de.uni_hannover.se.pdfzensor.Logging;
+import de.uni_hannover.se.pdfzensor.testing.TestUtility;
 import de.uni_hannover.se.pdfzensor.testing.argumentproviders.TokenProvider;
-import org.apache.commons.lang3.ArrayUtils;
 import org.apache.logging.log4j.Level;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.text.Normalizer;
+import java.util.ArrayDeque;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
+import static java.text.Normalizer.Form.NFD;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TokenizerTest {
@@ -36,16 +43,51 @@ class TokenizerTest {
 	}
 	
 	@ParameterizedTest
+	@ValueSource(strings = {"", ".*", "(ab)", "ab|.*", "(?:a*)|b", "()", "(?)", "(?:)", "(", "\\", "[^3", "\\()"})
+	void testInvalidToken(String token) {
+		var method = TestUtility.getPrivateMethod(Tokenizer.class, "isTokenValid", String.class);
+		try {
+			assertEquals(Boolean.FALSE, method.invoke(null, token));
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			fail(e);
+		}
+	}
+	
+	@ParameterizedTest
+	@ValueSource(strings = {"a", ".+", "(?:xy)", "\\(\\)", "(?:)d+"})
+	void testValidToken(String token) {
+		var method = TestUtility.getPrivateMethod(Tokenizer.class, "isTokenValid", String.class);
+		try {
+			assertEquals(Boolean.TRUE, method.invoke(null, token));
+		} catch (IllegalAccessException | InvocationTargetException e) {
+			fail(e);
+		}
+	}
+	
+	@Test
+	void testLigatureTokenization() {
+		try (var tokenizer = new Tokenizer<>(new SimpleToken("a\u030A"))) {
+			var queue = "aaaa\u030Aaaaaaaa\u030Aaaa".chars().boxed().collect(Collectors.toCollection(ArrayDeque::new));
+			tokenizer.setHandler((value, payload, token) -> {
+				value = Normalizer.normalize(value, NFD);
+				assertEquals(queue.pop(), value.chars().findFirst().orElseThrow());
+			});
+			
+			tokenizer.input("aaa\u00E5aaa", Collections.nCopies(7, new Object()));
+			tokenizer.input("aaaa\u030Aaaa", Collections.nCopies(8, new Object()));
+		} catch (Exception e) {
+			fail(e);
+		}
+	}
+	
+	@ParameterizedTest
 	@ArgumentsSource(TokenProvider.class)
 	void simpleStreamTokenization(@NotNull String[][] inputs, String[] tokenized) {
-		var ref = new Object() {
-			int index = 0;
-		};
+		var queue = new ArrayDeque<>(List.of(tokenized));
 		try (var tokenizer = new Tokenizer<>(TestToken.values())) {
 			tokenizer.setHandler((token, payload, type) -> {
-				assertTrue(ArrayUtils.isArrayIndexValid(tokenized, ref.index));
-				assertEquals(tokenized[ref.index], token);
-				ref.index++;
+				assertFalse(queue.isEmpty());
+				assertEquals(queue.pop(), token);
 			});
 			
 			for (String[] input : inputs) {
@@ -57,7 +99,7 @@ class TokenizerTest {
 		} catch (IOException e) {
 			fail(e);
 		}
-		assertEquals(tokenized.length, ref.index);
+		assertTrue(queue.isEmpty());
 	}
 	
 	/**
@@ -78,6 +120,19 @@ class TokenizerTest {
 		}
 		
 		/** {@inheritDoc} */
+		@Override
+		public String getRegex() {
+			return regex;
+		}
+	}
+	
+	static class SimpleToken implements TokenDef {
+		private String regex;
+		
+		SimpleToken(String regex) {
+			this.regex = regex;
+		}
+		
 		@Override
 		public String getRegex() {
 			return regex;
