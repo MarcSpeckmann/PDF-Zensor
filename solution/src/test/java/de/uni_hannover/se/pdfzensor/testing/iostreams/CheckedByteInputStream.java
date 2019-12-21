28f13335643c816ee0256d0586a569998bb25f0c
Tim Hagen <timhagenmail@googlemail.com>
2019-12-21 09:59:07 +0100

    added further documentation

--- solution/src/test/java/de/uni_hannover/se/pdfzensor/testing/iostreams/CheckedByteInputStream.java
+++ solution/src/test/java/de/uni_hannover/se/pdfzensor/testing/iostreams/CheckedByteInputStream.java
@@ -7,16 +7,29 @@ import java.io.IOException;
 
 /** An extension of the ByteArrayInputStream to check if the stream was properly closed. */
 public class CheckedByteInputStream extends ByteArrayInputStream {
+	/** Stores if this stream was closed by a call to {@link #close()}. */
 	private boolean wasClosed = false;
 	
+	/**
+	 * Creates a new {@link ByteArrayInputStream} containing the provided data.
+	 *
+	 * @param buf the data used to populate the stream.
+	 * @see ByteArrayInputStream#ByteArrayInputStream(byte[])
+	 */
 	public CheckedByteInputStream(@NotNull final byte[] buf) {
 		super(buf);
 	}
 	
+	/**
+	 * Returns true iff this stream was closed previously.
+	 *
+	 * @return true iff this stream was closed previously.
+	 */
 	public boolean isClosed() {
 		return wasClosed;
 	}
 	
+	/** {@inheritDoc} */
 	@Override
 	public void close() throws IOException {
 		wasClosed = true;
