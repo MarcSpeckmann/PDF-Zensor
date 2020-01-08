5c5cdd718a817e1abec38d98b3f7e0bfa18a8012
Marc Speckmann <marc.speckmann@gmail.com>
2020-01-08 08:41:17 +0100

    Logger is logging exceptions on lvl error

--- solution/src/main/java/de/uni_hannover/se/pdfzensor/App.java
+++ solution/src/main/java/de/uni_hannover/se/pdfzensor/App.java
@@ -6,12 +6,15 @@ import de.uni_hannover.se.pdfzensor.config.CLErrorMessageHandler;
 import de.uni_hannover.se.pdfzensor.config.CLHelp;
 import de.uni_hannover.se.pdfzensor.config.Settings;
 import de.uni_hannover.se.pdfzensor.processor.PDFProcessor;
+import org.apache.logging.log4j.Logger;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import picocli.CommandLine;
 
 import java.io.IOException;
 
 public class App {
+	/** The Logger-instance instances of this class should log their output into. */
+	private static final Logger LOGGER = Logging.getLogger();
 	
 	@SuppressWarnings("squid:S106")// we explicitly want to print to stderr here instead of logging
 	public static void main(String[] args) {
@@ -27,9 +30,11 @@ public class App {
 			}
 		} catch (CommandLine.ParameterException ex) {
 			CLErrorMessageHandler handler = new CLErrorMessageHandler();
+			LOGGER.error(ex);
 			System.exit(handler.handleParseException(ex, args));
 		} catch (IOException e) {
 			System.err.println(e.getMessage());
+			LOGGER.error(e);
 			System.exit(-1);
 		}
 	}
