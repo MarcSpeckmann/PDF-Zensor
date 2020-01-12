27be9a7cec1abc755094afa1737d13a14572e218
siddhartha <maksim.gluzman@web.de>
2020-01-12 14:08:45 +0100

    PDF2-126 #comment add doc to PasswordUtil

--- solution/src/main/java/de/uni_hannover/se/pdfzensor/censor/utils/PasswordUtil.java
+++ solution/src/main/java/de/uni_hannover/se/pdfzensor/censor/utils/PasswordUtil.java
@@ -3,17 +3,33 @@ package de.uni_hannover.se.pdfzensor.censor.utils;
 
 import de.uni_hannover.se.pdfzensor.Logging;
 import de.uni_hannover.se.pdfzensor.config.Settings;
+import de.uni_hannover.se.pdfzensor.utils.RectUtils;
 import org.apache.logging.log4j.Level;
+import org.jetbrains.annotations.Contract;
 
 import java.util.Scanner;
 
-//TODO: add comments
+
+/**
+ * Utility-class for handling password input and missing or incorrect passwords.
+ */
 public final class PasswordUtil {
 
+	/**
+	 * This constructor should not be called as no instance of {@link PasswordUtil} shall be created.
+	 *
+	 * @throws UnsupportedOperationException when called
+	 */
+	@Contract(value = " -> fail", pure = true)
     private PasswordUtil(){
-        //
+		throw new UnsupportedOperationException();
     }
 
+	/**
+	 * @param settings The {@link Settings} that belong to the document that is encrypted..
+ 	 * @return A boolean that is used to determine, if the password-handling has been finished
+	 * by giving an empty password or if --no-interaction is set.
+	 */
     public static boolean handleIncorrectPassword(Settings settings){
         if(settings.getNoInteraction()){
             Logging.getLogger().log(Level.ERROR, "The document " + settings.getInput() + " is encrypted." +
@@ -25,6 +41,10 @@ public final class PasswordUtil {
         }
     }
 
+	/**
+	 * @param settings The {@link Settings} that belong to the document that is encrypted.
+	 * @return The password entered by the user as a String.
+	 */
     public static String getPasswordFromCL(Settings settings){
         Scanner scanner = new Scanner(System.in);
         Logging.getLogger().log(Level.ERROR, "Please enter the password for encrypted document {}", settings.getInput());
